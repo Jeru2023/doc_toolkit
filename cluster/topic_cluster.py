68cfce0e15a46ca95b43141191faac2a5cbade5b
liujeru <jeru.token@gmail.com>
2024-03-19 12:45:13 +0800

    test topic cluster

--- cluster/topic_cluster.py
+++ cluster/topic_cluster.py
@@ -6,16 +6,26 @@ from sentence_transformers import SentenceTransformer
 embedding_model = 'BAAI/bge-large-zh-v1.5'
 
 
-def tokenize_zh(text):
-    words = jieba.lcut(text)
-    return words
+class TopicCluster:
+
+    @staticmethod
+    def tokenize_zh(text):
+        words = jieba.lcut(text)
+        return ' '.join(words)
+
+    def cluster(self, docs):
+        content = '\n'.join(docs).strip()
+        vectorizer = CountVectorizer(tokenizer=lambda x: self.tokenize_zh(x))
+        topic_model = BERTopic(language="multilingual", embedding_model=embedding_model, verbose=True, vectorizer_model=vectorizer)
+
+        sentence_model = SentenceTransformer(embedding_model)
+        embeddings = sentence_model.encode(docs, show_progress_bar=False)
+        topics, _ = topic_model.fit_transform(docs, embeddings=embeddings)
+        return topics
 
 
-vectorizer = CountVectorizer(tokenizer=tokenize_zh)
-topic_model = BERTopic(language="multilingual", embedding_model=embedding_model, verbose=True, vectorizer_model=vectorizer, min_topic_size=3)
-
-
-docs = [
+if __name__ == '__main__':
+    docs = [
         "12月12日，阳光电源公告，公司控股股东、实际控制人曹仁贤将其持有本公司的380万股股份办理了质押业务，目前合计质押公司股份3277万股，占其所持股比7.22%。",
         "这只是一个案例而已。实际上，曹仁贤近年来依托公司主业，围绕光伏与储能、半导体等产业链，全面展开个人投资业务，完全媲美顶级投资机构，堪称一代大师。",
         "今年的胡润百富榜上，阳光电源董事长曹仁贤及夫人苏蕾，以480亿位列第86位，蝉联安徽省首富。胡润百富统计曹老师的财富，只采集了其持有的阳光电源股权的信息，这其实并不全面。",
@@ -29,9 +39,8 @@ docs = [
         "这就是三星的担忧所在，它建议员工不要在周末与家人和朋友外出，要求他们戴上口罩，相互交谈时保持2米以上的距离。",
         "根据极光iapp数据显示，从生鲜电商app渗透率来看，排名前五的主要是以线下社区型为主，包括生鲜O2O型、社区便利店和新零售，当然也包括部分仅有线上运营的前置仓模式的叮咚买菜和朴朴。",
         "可以预见的是，社区生鲜电商行业将是一场持久战，究其原因在于中国地域广阔，生鲜线下实体店/市场作为国民基础设施区域布局完善程度不同，各线城市对于生鲜需求倾向性有较大的区别。"
-        ]
-sentence_model = SentenceTransformer(embedding_model)
-embeddings = sentence_model.encode(docs, show_progress_bar=False)
-topics, _ = topic_model.fit_transform(docs, embeddings=embeddings)
+    ]
 
-print(topics)
+    topic_cluster = TopicCluster()
+    topics = topic_cluster.cluster(docs)
+    print(topics)
