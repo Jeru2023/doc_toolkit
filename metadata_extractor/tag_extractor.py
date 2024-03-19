35bb056d2ec0a3f37987ead72fcae704653a4ecb
Jeru2023 <jeru.token@gmail.com>
2024-03-20 03:02:32 +0800

    suppport text_rank extractor

--- metadata_extractor/tag_extractor.py
+++ metadata_extractor/tag_extractor.py
@@ -7,18 +7,18 @@ POS = ('n', 'nz', 'ns', 'nt', 'nr', 'l')
 class TagExtractor:
 
     @staticmethod
-    def extract(text, top_k=5, text_rank=False):
-        if text_rank:
-            tags = jieba.analyse.textrank(text, topK=top_k, withWeight=False, allowPOS=POS)
+    def extract(text, top_k=5, tf_idf=False):
+        if tf_idf:
+            return jieba.analyse.extract_tags(text, topK=top_k, allowPOS=POS)
+
         else:
-            # TF-IDF
-            tags = jieba.analyse.extract_tags(text, topK=top_k, allowPOS=POS)
-        return tags
+            # text rank
+            return jieba.analyse.textrank(text, topK=top_k, withWeight=False, allowPOS=POS)
 
 
 if __name__ == '__main__':
     tag_extractor = TagExtractor()
 
     text = "美元指数回落至104下方，非美货币涨跌互现，人民币小幅贬值：人民币即期汇率收于7.1984（+57pips），日元-0.22%、韩元+0.33%、欧元+0.42%、加元+0.06%、澳元+0.83%、英榜+0.43%。"
-    tags = tag_extractor.extract(text, text_rank=True)
+    tags = tag_extractor.extract(text)
     print(tags)
